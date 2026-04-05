c81e5f2674a3287c44c2618ea0796b062c48b560
Umair Butt <umairbutt00007@gmail.com>
2026-04-06 02:57:09 +0500

    fixed eks

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,41 +1,91 @@
+# EKS CLUSTER ROLE
 resource "aws_iam_role" "eks_cluster" {
- name = "eks-cluster-role"
+  name = "eks-cluster-role"
 
- assume_role_policy = jsonencode({
-  Version = "2012-10-17"
-  Statement = [{
-   Effect = "Allow"
-   Principal = {
-    Service = "eks.amazonaws.com"
-   }
-   Action = "sts:AssumeRole"
-  }]
- })
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
 }
 
+# POLICY FOR CLUSTER
+resource "aws_iam_role_policy_attachment" "cluster_policy" {
+  role       = aws_iam_role.eks_cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+# EKS CLUSTER
 resource "aws_eks_cluster" "cluster" {
- name = "terraform-eks"
+  name     = "terraform-eks"
+  role_arn = aws_iam_role.eks_cluster.arn
 
- role_arn = aws_iam_role.eks_cluster.arn
+  vpc_config {
+    subnet_ids = var.subnet_ids
+  }
 
- vpc_config {
-  subnet_ids = var.subnet_ids
- }
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster_policy
+  ]
 }
 
+# NODE ROLE (EC2)
+resource "aws_iam_role" "eks_nodes" {
+  name = "eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+# NODE POLICIES
+resource "aws_iam_role_policy_attachment" "node_worker_policy" {
+  role       = aws_iam_role.eks_nodes.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "node_cni_policy" {
+  role       = aws_iam_role.eks_nodes.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "node_registry_policy" {
+  role       = aws_iam_role.eks_nodes.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+# NODE GROUP
 resource "aws_eks_node_group" "nodes" {
- cluster_name = aws_eks_cluster.cluster.name
- node_group_name = "worker-nodes"
+  cluster_name    = aws_eks_cluster.cluster.name
+  node_group_name = "worker-nodes"
 
- node_role_arn = aws_iam_role.eks_cluster.arn
+  node_role_arn = aws_iam_role.eks_nodes.arn
 
- subnet_ids = var.subnet_ids
+  subnet_ids = var.subnet_ids
 
- scaling_config {
-  desired_size = 1
-  max_size     = 1
-  min_size     = 1
- }
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
 
- instance_types = ["t3.micro"]
+  instance_types = ["t3.micro"]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.node_worker_policy,
+    aws_iam_role_policy_attachment.node_cni_policy,
+    aws_iam_role_policy_attachment.node_registry_policy
+  ]
 }
