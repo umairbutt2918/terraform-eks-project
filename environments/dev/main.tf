18e62a04ece7981ac4766a3c1882100b9d7d51f7
Umair Butt <umairbutt00007@gmail.com>
2026-04-06 02:18:12 +0500

    fix terraform formatting

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,33 +1,33 @@
 terraform {
 
- backend "s3" {
-   bucket = "terraform-eks-state-demo-123456"
-   key    = "dev/terraform.tfstate"
-   region = "us-east-1"
+  backend "s3" {
+    bucket = "terraform-eks-state-demo-123456"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
 
-   dynamodb_table = "terraform-locks"
- }
-
- required_providers {
-  aws = {
-   source  = "hashicorp/aws"
-   version = "~> 5.0"
+    dynamodb_table = "terraform-locks"
   }
- }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 
 }
 
 provider "aws" {
- region = "us-east-1"
+  region = "us-east-1"
 }
 
 module "network" {
- source = "../../modules/network"
+  source = "../../modules/network"
 }
 
 module "eks" {
- source = "../../modules/eks"
+  source = "../../modules/eks"
 
- vpc_id     = module.network.vpc_id
- subnet_ids = module.network.private_subnets
+  vpc_id     = module.network.vpc_id
+  subnet_ids = module.network.private_subnets
 }
