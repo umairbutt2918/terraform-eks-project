ed063a55e1b68d66853ec97afcc61ee9eb9361fe
Umair Butt <umairbutt00007@gmail.com>
2026-04-06 02:33:40 +0500

    fix network module for EKS multi AZ

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,57 +1,79 @@
 resource "aws_vpc" "main" {
- cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
 }
 
+# PUBLIC SUBNET
 resource "aws_subnet" "public" {
- vpc_id = aws_vpc.main.id
- cidr_block = "10.0.1.0/24"
- map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "private" {
- vpc_id = aws_vpc.main.id
- cidr_block = "10.0.2.0/24"
+# PRIVATE SUBNET 1
+resource "aws_subnet" "private1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
 }
 
+# PRIVATE SUBNET 2
+resource "aws_subnet" "private2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
+}
+
+# INTERNET GATEWAY
 resource "aws_internet_gateway" "gw" {
- vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
 }
 
+# ELASTIC IP FOR NAT
 resource "aws_eip" "nat_eip" {
- domain = "vpc"
+  domain = "vpc"
 }
 
+# NAT GATEWAY
 resource "aws_nat_gateway" "nat" {
- allocation_id = aws_eip.nat_eip.id
- subnet_id     = aws_subnet.public.id
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public.id
 }
 
+# PUBLIC ROUTE TABLE
 resource "aws_route_table" "public_rt" {
- vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
 }
 
 resource "aws_route" "public_internet" {
- route_table_id = aws_route_table.public_rt.id
- destination_cidr_block = "0.0.0.0/0"
- gateway_id = aws_internet_gateway.gw.id
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
 }
 
+# PRIVATE ROUTE TABLE
 resource "aws_route_table" "private_rt" {
- vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
 }
 
 resource "aws_route" "private_nat" {
- route_table_id = aws_route_table.private_rt.id
- destination_cidr_block = "0.0.0.0/0"
- nat_gateway_id = aws_nat_gateway.nat.id
+  route_table_id         = aws_route_table.private_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
 }
 
+# ROUTE TABLE ASSOCIATIONS
 resource "aws_route_table_association" "public_assoc" {
- subnet_id      = aws_subnet.public.id
- route_table_id = aws_route_table.public_rt.id
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
 }
 
-resource "aws_route_table_association" "private_assoc" {
- subnet_id      = aws_subnet.private.id
- route_table_id = aws_route_table.private_rt.id
+resource "aws_route_table_association" "private_assoc1" {
+  subnet_id      = aws_subnet.private1.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_assoc2" {
+  subnet_id      = aws_subnet.private2.id
+  route_table_id = aws_route_table.private_rt.id
 }
